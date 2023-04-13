dd18335aa1da63e598d8e9c098102a85d4987db1
Chiara Mazzeo <chiara.mazzeo@mail.polimi.it>
2023-04-13 18:56:04 +0200

    Update 1

--- Assets/_Scripts/ClickInputManager.cs
+++ Assets/_Scripts/ClickInputManager.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ClickInputManager : MonoBehaviour
 {
 
     public GameObject panelToAppear;
+    public TextMeshProUGUI objectName, objectDescription, numberOfCompletedText;
+
+    public bool[] objectsCompleted;
+    public int numberOfCompleted;
 
     // Start is called before the first frame update
     void Start()
@@ -19,16 +24,66 @@ public class ClickInputManager : MonoBehaviour
         Debug.LogError(Input.touches);
         foreach (Touch touch in Input.touches)
         {
-            
+
             if (touch.phase == TouchPhase.Began)
             {
+                RaycastHit hit;
                 // Construct a ray from the current touch coordinates
                 Ray ray = Camera.main.ScreenPointToRay(touch.position);
-                if (Physics.Raycast(ray))
+                if (Physics.Raycast(ray, out hit))
                 {
-                    // Create a particle if hit
-                    panelToAppear.SetActive(true);
+                    if (hit.collider != null)
+
+                        switch (hit.collider.name)
+                        {
+                            case "GPS":
+
+                                objectName.SetText("GPS");
+                                objectDescription.SetText("GPS description. Bla bla bla");
+
+                                panelToAppear.SetActive(true);
+
+                                objectsCompleted[0] = true;
+
+                                break;
+
+                            case "Mask":
+
+                                objectName.SetText("Surgeon");
+                                objectDescription.SetText("Mask description");
+
+                                panelToAppear.SetActive(true);
+
+                                objectsCompleted[1] = true;
+
+                                break;
+
+                            case "Trophy":
+
+                                objectName.SetText("Trophy");
+                                objectDescription.SetText("Trophy description. Bla bla bla as well");
+
+                                panelToAppear.SetActive(true);
+
+                                objectsCompleted[2] = true;
+
+                                break;
+                        }
+
+                    numberOfCompleted = 0;
+
+                    for (int i = 0; i < objectsCompleted.Length; i++)
+                    {
+                        if(objectsCompleted[i] == true)
+                        {
+                            numberOfCompleted++;
+                        }
+                    }
+
+                    numberOfCompletedText.SetText(numberOfCompleted + " / " + objectsCompleted.Length);
                 }
+
+               
             }
         }
 
